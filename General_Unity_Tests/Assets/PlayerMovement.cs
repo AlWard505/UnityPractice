5f149a3beda58c7469a880eedd998cb185d57989
epicpigslav688 <epicpigslav@gmail.com>
2021-06-29 13:37:18 +0100

    Initial commit

---
+++ General_Unity_Tests/Assets/PlayerMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    public CharacterController2D Controller;
+    //[SerializeField] player Player;
+    public float runspeed = 40f;
+    public int PlayerLives = 3;
+    //public GetComponent.boxCollider2D Collision2D;
+    float horizontalMove = 0f;
+
+    bool jump = false;
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runspeed;
+        
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        //moves the character
+        Controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+
+    void death()
+    {
+        if (PlayerLives < 1)
+        {
+            //GetComponent < BoxCollider2D > "spikes";
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("kinda");
+        if (other.gameObject.tag == "Enemy")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        else if (other.gameObject.tag == "bullet")
+        {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
+ 
---
+++ General_Unity_Tests/Assets/ignorecollision.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ignorecollision : MonoBehaviour
+{
+    public GameObject bullet;
+    public GameObject wall;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Physics2D.IgnoreCollision(bullet.GetComponent<Collider2D>(), wall.GetComponent<Collider2D>(), true);
+    }
+}
